68125acb52151afc475ed61752528b9be6a2213d
Yerden Dorjiyev <erdorjiev@gmail.com>
2022-10-10 01:53:05 +0300

    final init

--- script.js
+++ script.js
@@ -14,23 +14,51 @@ firstInput.addEventListener
 secondInput.addEventListener
 ("keypress", function(event) 
   {
-    if (firstInput.value=="")
-    {
-      firstInput.focus()
-    }
+  if (firstInput.value=="")
+  {
+    firstInput.focus()
+  }
   else if (event.key === "Enter" && secondInput.value!=="")
-    {
-      butt.click();
-    }
+  {
+    butt.click();
+  }
   }
 )
 
 butt.onclick = function clickAction()
   {
-    document.getElementById('result').innerText = comparison(firstInput.value,secondInput.value)
+  document.getElementById('result').innerText = comparison(firstInput.value,secondInput.value)
   }
 
 function comparison(str1,str2)
+{
+str1 = " " + str1
+str2 = " " + str2
+let n1 = str1.length;
+let n2 = str2.length;
+var p = new Array(n1);
+  for (let z = 0; z < n1; z++)
   {
-    for(let i=0,i<str1.length,)
+    p[z] = new Array(n2);
   }
+  
+for(let i=0;i<n1;i++)
+  {
+  for(let j=0;j<n2;j++)
+    {
+    if (i == 0 )
+      {p[i][j]=i}
+    else if ( j == 0 )
+      {p[i][j]=j}
+    else if (str1[i] == str2[j])
+      {
+      p[i][j] = p[i-1][j-1] ;
+      }
+    else 
+      {
+      p[i][j] = Math.min(p[i-1][j-1]+1,p[i-1][j]+1,p[i][j-1]+1);
+      }
+    }
+  }
+return p[n1-1][n2-1]
+}
